90fc398853761776b3a648a3fab2908bdfb13f3a
GabrielJMJ <gamjj74@hotmail.com>
2014-05-07 21:18:50 -0300

    Set name of method to render templates

--- Application/MVC/Controller/Index.php
+++ Application/MVC/Controller/Index.php
@@ -2,10 +2,15 @@
     namespace MVC\Controller;
     
     use Attw\Controller\AbstractController;
+    use Attw\View\Html\Element\Form;
+    use Attw\HTTP\Request;
+    use Attw\File\File;
+    use Attw\File\Validator\Extension;
+    use Attw\File\Uploader\Upload;
 
     class Index extends AbstractController{
         public function index(){
-		$vars = [ 'message' => 'This message was puted here with <b>Smarty</b>.' ];
-		$this->view( 'index.html', $vars );
+            $vars = [ 'message' => 'This message was puted here with <b>Smarty</b>.' ];
+            $this->render( 'index.html', $vars );
         }
     }
