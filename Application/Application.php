48c77f6ca7a0f27c537970bb0d8bef062d641be3
GabrielJMJ <gamjj74@hotmail.com>
2014-06-15 01:47:24 -0300

    Set application implementation

--- Application/Application.php
+++ Application/Application.php
@@ -1,6 +1,12 @@
 <?php
-	/**
-     * Execute the application
-    */
-    $application = new Attw\Application\Application();
-    $application->execute();
+	use Attw\Application\Application;
+	use Attw\Router\RoutingHandler;
+	use Attw\Application\ControllerDispatcher;
+	use Attw\HTTP\Request;
+
+	require_once 'Configurations' . DIRECTORY_SEPARATOR . 'routes.php';
+
+	$routingHandler = new RoutingHandler( $routesCollection );
+
+    $application = new Application( new ControllerDispatcher(), $routingHandler );
+    $application->run( new Request(), 'MVC\Controller' );
--- Application/Configurations/routes.php
+++ Application/Configurations/routes.php
@@ -1,11 +1,19 @@
 <?php
-    use Attw\Router\RoutingManager;
-
-    $routing = RoutingManager::getInstance();
+    use Attw\Router\Route;
+    use Attw\Router\RoutesCollection;
 
     /**
-     * $routing->add( 'id', [ 
-     * 'controller' => [ 'post' => 'Posts' ] 
-     * 'action' => 'read'
-     * ] );
+     * If you change the name of this variable, 
+     *  go to Application/Application.php and change there too
+    */
+    $routesCollection = new RoutesCollection();
+
+    /**
+     * @example
+     *  $routesCollection->add( new Route( 'post_read', 'id', array(
+     * 	    'controller' => array(
+     * 		   'post' => 'Posts'
+     * 	    ),
+     * 	    'action' => 'read'
+     *  ) ) );
     */
