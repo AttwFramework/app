7ba3773c0eee7b021e7357e31bb2818e41b996ec
GabrielJMJ <gamjj74@hotmail.com>
2014-06-25 17:55:07 -0300

    Remove logger from Index controller

--- Application/MVC/Controller/Index.php
+++ Application/MVC/Controller/Index.php
@@ -2,13 +2,9 @@
     namespace MVC\Controller;
     
     use Attw\Controller\AbstractController;
-    use Attw\Logger\FileLogger;
-    use Attw\Logger\LogLevel;
 
     class Index extends AbstractController{
         public function index(){
             $this->render( 'index.html' );
-            FileLogger::setFile( 'public/logs.txt' );
-            FileLogger::write( LogLevel::ALERT, 'Controller: Index Action: Index' );
         }
     }
